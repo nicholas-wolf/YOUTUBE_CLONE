299cac464f186982bcb2fa06bae7ac7835e8696e
nicholas-wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-05-11 21:23:46 -0400

    Created Reply class in Reply app models.py

--- backend/Reply/models.py
+++ backend/Reply/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    
