03f749ccd6487b561f72b2ce7507b57468940e91
nicholas-wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-05-11 21:34:20 -0400

    Changed Reply class in Reply app models.py

--- backend/Reply/models.py
+++ backend/Reply/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from authentication.models import User
+from Comment.models import Comment
 
 # Create your models here.
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     
