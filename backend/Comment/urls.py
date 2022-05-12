35fd3040d80552cd8a844ab2f379dd3688fbeeb7
nicholas-wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-05-12 15:17:09 -0400

    Updated GET, POST, and PUT functions in Comment views.py

--- backend/Comment/urls.py
+++ backend/Comment/urls.py
@@ -4,6 +4,8 @@ from . import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('', views.user_comments),
-    path('all/', views.get_all_comments),
+    path('', views.get_all_comments),
+    path('all/<int:pk>/', views.get_all_comments),
+    path('update/<int:pk>/', views.edit_comment),
+    path('post/', views.user_comment)
 ]
--- backend/Comment/views.py
+++ backend/Comment/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -10,15 +11,27 @@ from .serializers import CommentSerializer
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def get_all_comments(request):
-    comments = Comment.objects.all()
+def get_all_comments(request, pk):
+    comments = Comment.objects.filter(video_id=pk)
     serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+@permission_classes([AllowAny])
+def edit_comment(request, pk):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}") 
+    comment = get_object_or_404(Comment, pk=pk)  
+    serializer = CommentSerializer(comment, data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST', 'PUT'])
+@api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def user_comments(request):
+def user_comment(request):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
     if request.method == 'POST':
@@ -31,9 +44,3 @@ def user_comments(request):
         comments = Comment.objects.filter(user_id=request.user.id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CommentSerializer(comments, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()    
-
-    
