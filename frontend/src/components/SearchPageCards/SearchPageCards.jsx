e1785b88ec57d66f084c73513689669ac1c4cd3e
nicholas-wolf <nicholas.r.wolf@gmail.com>
2022-05-13 17:53:13 -0400

    test

---
+++ frontend/src/components/SearchPageCards/SearchPageCards.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            TODO put video discription here
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}test
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
