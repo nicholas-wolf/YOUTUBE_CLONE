e2a3e68d395c5d796804b36194552c44061b28fb
michael-nevius <michael.a.nevius@gmail.com>
2022-05-13 15:50:25 -0400

    Created comment form for videos

--- frontend/src/components/CommentForm/CommentForm.jsx
+++ frontend/src/components/CommentForm/CommentForm.jsx
@@ -1,40 +1,60 @@
-import axios from "axios";
-import React, { useState } from "react";
 
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
+class CommentCreator extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: "",
+            videoID: "",
+        }
+    }
 
-const CreateComment = (props) => {
-  
-  const [commentText, setCommentText] = useState("");
-  
-  async function handleSubmit(event) {
-    
-    event.preventDefault();
+    addComment = async () => {
+        let comment = {
+            text: this.state.text,
+            videoID: "M7lc1UVf-VE"
+        } 
+        try {
+            await axios.post('http://localhost:3000/api/comments/', comment)
+            .then((res) => console.log(res.data))
+            this.setState({
+                name: '',
+                comment: '',
+            });
+        }
+        catch (err) {
+            alert(err);
+        }
+    }
 
-    let newComment = {
-      text: commentText,
-      videoID: "M7lc1UVf-VE",
-    };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-    await axios
-      .post("http://localhost:3000/api/comments", newComment)
-      .then((res) => props.AddNewComment(res.data));
-  }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.addComment();
+    }
 
-  return (
-    
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter a comment"
-        value={commentText}
-        onChange={(event) => setCommentText(event.target.value)}
-      ></input>
-      <span>
-        <button type="submit">SEND</button>
-      </span>
-    </form>
-  );
-};
+    render() { 
+        return ( 
+            <div className="container jumbotron small" style={{maxHeight: '30vh'}}>
+                <h3>Comment</h3>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="comment">
+                        <Form.Control type="text" placeholder="Leave a comment..." name='text' value={this.state.text} onChange={this.handleChange}/>
+                    </Form.Group>           
+                    <Button variant="primary" type="submit">Add Comment</Button>
+                </Form>
+            </div>
+         );
+    }
+}
 
-export default CreateComment;
+export default CommentCreator;
