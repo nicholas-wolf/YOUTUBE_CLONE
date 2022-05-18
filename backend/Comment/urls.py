7b19e45de4b10af98b1ed476b34cc0eb9f957884
nicholas-wolf <nicholas.r.wolf@gmail.com>
2022-05-18 10:40:21 -0400

    changed tab name to Youtube Clone

--- backend/Comment/urls.py
+++ backend/Comment/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.get_all_comments),
-    path('all/<int:pk>/', views.get_all_comments),
+    path('all/', views.get_all_comments),
     path('update/<int:pk>/', views.edit_comment),
     path('post/', views.user_comment)
 ]
